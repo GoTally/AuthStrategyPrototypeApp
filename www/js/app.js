69e1d8d8d3e67e2bfff6148f953708309da86715
Tyler Ewing <tewing10@gmail.com>
2015-02-02 18:43:55 -0500

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    // Facebook stuff
+    if (window.cordova.platformId == "browser") {
+      facebookConnectPlugin.browserInit("1580290688872529");
+    }
+  });
+})
+
+app.controller("BaseController", function() {
+  this.userId = '';
+  this.response = '';
+
+  this.login = function() {
+    var self = this;
+    console.log('login');
+    facebookConnectPlugin.login(['email', 'user_friends'], function(response) {
+      self.response = JSON.stringify(response);
+      self.userId = response.authResponse.userID;
+    });
+  };
+
+  this.getLoginStatus = function() {
+    var self = this;
+    console.log('get login status');
+    facebookConnectPlugin.getLoginStatus(function(response) {
+      self.response = JSON.stringify(response);
+      self.userId = response.authResponse.userID;
+    });
+  };
+});
